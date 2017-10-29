1dd3d3bcd2a7dbc769c42904bab5e208484cf0d2
Marco Giebel <dev-github@higger.de>
2017-10-29 11:23:47 +0100

    Schedule data import

---
+++ backend/src/main/java/de/higger/fwutils/wiki/config/ScheduleConfiguration.java
@@ -0,0 +1,9 @@
+package de.higger.fwutils.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class ScheduleConfiguration {
+}
